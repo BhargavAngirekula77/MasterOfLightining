da588753529bca6580e93186b94f63e7369fa7c6
angir <angir@GABRIEL>
2023-11-17 10:49:38 +0530

    Commit_2 sending the chasges

---
+++ src/test/java/demo_thing/Rockit_New.java
@@ -0,0 +1,10 @@
+package demo_thing;
+
+public class Rockit_New {
+
+	public static void main(String[] args) {
+		System.out.println("this is the part where we send chages to the git hub");
+
+	}
+
+}
