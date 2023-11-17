8ac415bc1a420f0af1d86150faeb62c6a0f44d46
angir <angir@GABRIEL>
2023-11-17 12:07:16 +0530

    Commit & pushing changes to git hub

--- src/test/java/demo_thing/Rockit_New.java
+++ src/test/java/demo_thing/Rockit_New.java
@@ -4,7 +4,21 @@ public class Rockit_New {
 
 	public static void main(String[] args) {
 		System.out.println("this is the part where we send chages to the git hub");
-
+		
 	}
+		int count=0;
+		int retryAnalyzer=5;
+
+		public boolean retry() {
+			while (count<retryAnalyzer) {
+				count++;
+				return true;
+				
+			}
+			
+			return false;
+		}
+
+	
 
 }
